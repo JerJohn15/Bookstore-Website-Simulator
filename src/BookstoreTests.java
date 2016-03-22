4c8edd6f0eb6e748c4b97ccfe1bf44c9eba04d04
JerJohn15 <tjon2013@bu.edu>
2016-03-22 19:56:51 -0400

    Added Junit Test config files
    - Configuration files needed to run Junit
    - Test file that will contain all tests performed

---
+++ src/BookstoreTests.java
@@ -0,0 +1,38 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BookstoreTests {
+	/**
+	 *  This file contains all of the tests being performed. 
+	 *  
+	 *  All user tests are performed within the 'UserTests' class, 
+	 *  while the Functional Tests, are done in the 'Functional Tests'
+	 *  file. 
+	 * @author Jeremiah
+	 *
+	 */
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+	
+	
+	public class UserTests{
+	
+		//insert test cases for user here 
+		
+		
+	}
+	
+	
+	
+	public class FunctionalTests{
+		
+		//insert test cases for the backend here. 
+		
+		
+	}
+	
+}
